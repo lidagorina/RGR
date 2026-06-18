e260b19d8a835014f32eaea74be8c30612fe77ab
lidagorina <lida.gorina.07@gmail.com>
2026-06-18 19:16:14 +0700

    промежуточные исправления

---
+++ chacha20.h
@@ -0,0 +1,14 @@
+#ifndef CHA_H
+#define CHA_H
+using namespace std;
+
+#include<iostream>
+#include<codecvt>
+#include<string>
+#include <fstream>
+#include<locale>
+#include <vector> 
+#include <set>
+void ChaCha20_fun(string original, int cipher_choice);
+
+#endif
